3bd75a3beea7fb63ce07989d87fb576caf0ce20c
phuonght017 <phuonght.hp@gmail.com>
2022-02-17 19:50:25 +0300

    add test to main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,6 @@
 require_relative 'license'
 require_relative 'flussonic_last_version'
 
-flussonic_last_version = FlussonicLastVersion.new.get
-
 first_lic = License.new('04.07.2021', nil, nil, '21.10')
 p first_lic.list_available_versions # -> ["21.07", "21.06"]
 
@@ -11,3 +9,8 @@ p second_lic.list_available_versions # -> ["20.02"]
 
 third_lic = License.new('04.03.2019', '20.02', '19.06', '20.10')
 p third_lic.list_available_versions # -> []
+
+flussonic_last_version = FlussonicLastVersion.new.get
+test_lic = License.new('04.07.2021', '20.02', '19.01', flussonic_last_version)
+p flussonic_last_version
+p test_lic.list_available_versions
