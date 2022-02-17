baf16e960d8c48cd9cb0c57e67e1262f220ca36f
phuonght017 <phuonght.hp@gmail.com>
2022-02-17 19:33:52 +0300

    Add results in main.rb and edit README.md

--- main.rb
+++ main.rb
@@ -2,12 +2,12 @@ require_relative 'license'
 require_relative 'flussonic_last_version'
 
 flussonic_last_version = FlussonicLastVersion.new.get
-puts 'First test:'
+
 first_lic = License.new('04.07.2021', nil, nil, '21.10')
-p first_lic.list_available_versions
-puts 'Second test:'
+p first_lic.list_available_versions # -> ["21.07", "21.06"]
+
 second_lic = License.new('04.07.2020', '20.02', '19.01', '20.10')
-p second_lic.list_available_versions
-puts 'Third test:'
+p second_lic.list_available_versions # -> ["20.02"]
+
 third_lic = License.new('04.03.2019', '20.02', '19.06', '20.10')
-p third_lic.list_available_versions
+p third_lic.list_available_versions # -> []
